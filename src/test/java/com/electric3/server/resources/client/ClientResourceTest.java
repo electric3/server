964665d6f4cb98cb4364366bf91ac0dfe313d5d5
Ivan Alyakskin <ivan_aliakskin@epam.com>
2016-02-21 01:12:40 +0300

    full scenario draft impl

--- src/test/java/com/electric3/server/resources/client/ClientResourceTest.java
+++ src/test/java/com/electric3/server/resources/client/ClientResourceTest.java
@@ -29,11 +29,51 @@ import static org.junit.Assert.assertTrue;
 public class ClientResourceTest extends JerseyTest {
     @Override
     protected Application configure() {
-        return new ResourceConfig(ClientsResource.class, DepartmentsResource.class, UsersResource.class);
+        return new ResourceConfig(ClientsResource.class, DepartmentsResource.class, UsersResource.class, ProjectsResource.class);
     }
 
     @Test
     public void testForDirector() {
+        ivanScenario();
+    }
+
+    public void ivanScenario() {
+        Holder<User> users = getUsers();
+        List<User> items = users.getItems();
+        User targetUser = null;
+        for( User user : items ) {
+            if( user.getEmail().equals("ivan@handyassist.com") ) {
+                targetUser = user;
+                break;
+            }
+        }
+
+        createClient(targetUser);
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        final String client_response_str =
+                target("clients")
+                        .path("info")
+                        .path(targetUser.getUser_id())
+                        .request(MediaType.APPLICATION_JSON_TYPE)
+                        .get(String.class);
+        assertNotNull(client_response_str);
+        System.out.println(" " + client_response_str);
+        Client client = Client.deserialize(client_response_str, Client.class);
+        assertNotNull(client);
+        Mock.ME.setClient(client);
+
+        usersScenario(getClientId());
+        departmentsScript(getClientId());
+    }
+
+    public void fullScenario() {
         createUser();
         try {
             Thread.sleep(3000);
@@ -164,7 +204,7 @@ public class ClientResourceTest extends JerseyTest {
         Holder<Department> forDepP = getDepartments();
         List<Department> deps = forDepP.getItems();
         for (Department department : deps) {
-            projectScript(clientId, (String) department.get_id());
+            departmentScenario(clientId, (String) department.get_id());
         }
     }
 
@@ -173,6 +213,8 @@ public class ClientResourceTest extends JerseyTest {
         newDep.setDescription("this is description " + new Date(System.currentTimeMillis()).toLocaleString());
         newDep.setOwner(owner);
         newDep.setClientId((String) Mock.ME.getClient().get_id());
+        newDep.setProgress(randomProgress());
+        newDep.setStatus(randomStatus());
         newDep.setTitle("this is title " + new Date(System.currentTimeMillis()).toLocaleString());
 
         Response newDepartmentResponse = target("clients")
@@ -197,11 +239,19 @@ public class ClientResourceTest extends JerseyTest {
 
     /******************************************************************************************************************/
 
-    public void projectScript(String clientId, String departmentId) {
+    public void departmentScenario(String clientId, String departmentId) {
         int before = getProjectsDepartment(departmentId).getItems().size();
-        createProjectForDepartment(clientId, departmentId);
-        int after = getProjectsDepartment(departmentId).getItems().size();
-        assertTrue((after - before) == 1);
+        if( before < 6 ) {
+            createProjectForDepartment(clientId, departmentId);
+            int after = getProjectsDepartment(departmentId).getItems().size();
+            assertTrue((after - before) == 1);
+        }
+
+        List<Project> items = getProjectsDepartment(departmentId).getItems();
+        for(Project project : items) {
+            projectScenario((String)project.get_id(), clientId);
+        }
+
     }
 
     public Holder<Project> getProjectsDepartment(String departmentId) {
@@ -218,6 +268,8 @@ public class ClientResourceTest extends JerseyTest {
         project.setDescription("Description " + getTSS());
         project.setDeadline(String.valueOf(System.currentTimeMillis() + 60 * 60 * 1000 * 12));
         project.setDepartmentId(departmentId);
+        project.setProgress(randomProgress());
+        project.setStatus(randomStatus());
 
         Holder<User> users = getUsers(clientId);
         int size = users.getItems().size();
@@ -228,6 +280,75 @@ public class ClientResourceTest extends JerseyTest {
                 .request(MediaType.APPLICATION_JSON_TYPE)
                 .post(Entity.entity(project.serialize(), MediaType.APPLICATION_JSON_TYPE), Response.class);
         assertNotNull(response);
+    }
+
+    /******************************************************************************************************************/
+
+    public void projectScenario(String projectId, String clientId) {
+        Holder<Delivery> deliveries = getDeliveries(projectId);
+        int before = deliveries.getItems().size();
+
+        if( before < 6 ) {
+            createDelivery(projectId, clientId);
+        }
+
+        deliveries = getDeliveries(projectId);
+        List<Delivery> items = deliveries.getItems();
+        for( Delivery delivery : items ) {
+            deliveryScenario((String)delivery.get_id(), clientId);
+        }
+    }
+
+    public Holder<Delivery> getDeliveries(String projectId) {
+        // projects                                               deliveries
+        String dlv_str = target("projects").path(projectId).path("deliveries").request().get(String.class);
+        Type fooType = new TypeToken<Holder<Delivery>>() {}.getType();
+        Holder<Delivery> usersHolder = new Gson().fromJson(dlv_str, fooType);
+        return usersHolder;
+    }
+
+    public void createDelivery(String projectId, String clientId) {
+        Holder<User> users = getUsers(clientId);
+        User randomUser = getRandomUser(users);
+        Delivery delivery = new Delivery();
+        delivery.setAssignee(randomUser);
+        delivery.setTitle( "D t " + getTSS() );
+        delivery.setDescription( "Description " + getTSS() );
+        delivery.setAssignee( randomUser );
+        delivery.setProgress( randomProgress() );
+        delivery.setStatus( randomStatus() );
+    }
+
+    /******************************************************************************************************************/
+
+    public void deliveryScenario(String deliveryId, String clientId) {
+        Holder<User> users = getUsers(clientId);
+        User randomUser = getRandomUser(users);
+        deliveryAddComment("Comment " + getTSS(), deliveryId, randomUser );
+    }
+
+    public Delivery getDelivery(String deliveryId) {
+        String str_delivery = target("deliveries").path(deliveryId).request().get(String.class);
+        Delivery delivery = Delivery.deserialize(str_delivery, Delivery.class);
+        return delivery;
+    }
+
+    public void setStatus( ) {
+
+    }
+
+    public void setProgress( ) {
+
+    }
+
+    public void deliveryAddComment(String str_comment, String deliveryId, User author) {
+        Comment comment = new Comment();
+        comment.setComment(str_comment);
+        comment.setAuthor( author );
+        comment.setTimestamp( String.valueOf(System.currentTimeMillis()) );
+        Response response = target("deliveries").path(deliveryId).path("comment")
+        .request(MediaType.APPLICATION_JSON_TYPE)
+        .post(Entity.entity(comment, MediaType.APPLICATION_JSON_TYPE), Response.class );
     }
 
     /******************************************************************************************************************/
@@ -244,6 +365,27 @@ public class ClientResourceTest extends JerseyTest {
 
     public String getTSS() {
         return new Date(System.currentTimeMillis()).toLocaleString();
+    }
+
+    public User getRandomUser(Holder<User> users) {
+        int ownerId = ThreadLocalRandom.current().nextInt(0, users.getItems().size());
+        return users.getItems().get(ownerId);
+    }
+
+    public StatusEnum randomStatus( ) {
+        int status = ThreadLocalRandom.current().nextInt(0, 2);
+        switch (status) {
+            case 0:
+                return StatusEnum.RED;
+            case 1:
+                return StatusEnum.ORANGE;
+            default:
+                return StatusEnum.GREEN;
+        }
+    }
+
+    public int randomProgress( ) {
+        return ThreadLocalRandom.current().nextInt(0, 100);
     }
 
     public User getRandomUser() {
