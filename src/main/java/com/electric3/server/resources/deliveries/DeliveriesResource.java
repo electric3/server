dcbe6e90a94efacbf95a9d9e83b4dedc14f44f90
Stanislav Smirnov <stas.smirnov88@gmail.com>
2016-02-20 19:53:00 +0300

    deliveries: log

--- src/main/java/com/electric3/server/resources/deliveries/DeliveriesResource.java
+++ src/main/java/com/electric3/server/resources/deliveries/DeliveriesResource.java
@@ -19,6 +19,7 @@ public class DeliveriesResource {
     @GET
     @Path("{id}")
     public Response getDelivery(@PathParam("id") String deliveryId) {
+        log.info("get delivery by id");
         DeliveriesDBManager deliveriesDBManager = DeliveriesDBManager.getInstance();
         try {
             return Response.ok(deliveriesDBManager.getDelivery(deliveryId), MediaType.APPLICATION_JSON).build();
@@ -31,6 +32,7 @@ public class DeliveriesResource {
     @GET
     @Path("/{id}/comments/")
     public Response getDeliveryComments(@PathParam("id") String deliveryId) {
+        log.info("get delivery comments");
         DeliveriesDBManager deliveriesDBManager = DeliveriesDBManager.getInstance();
         try {
             return Response.ok(deliveriesDBManager.getDeliveryComments(deliveryId), MediaType.APPLICATION_JSON).build();
@@ -43,6 +45,7 @@ public class DeliveriesResource {
     @GET
     @Path("{id}/setStatus/{statusId}")
     public Response setStatus(@PathParam("id") String deliveryId, @PathParam("statusId") String statusId) {
+        log.info("set delivery status");
         DeliveriesDBManager deliveriesDBManager = DeliveriesDBManager.getInstance();
         try {
             deliveriesDBManager.setStatus(deliveryId, Integer.parseInt(statusId));
@@ -56,6 +59,7 @@ public class DeliveriesResource {
     @GET
     @Path("{id}/setProgress/{progressValue}")
     public Response setProgress(@PathParam("id") String deliveryId, @PathParam("progressValue") String progressValue) {
+        log.info("set delivery progress");
         DeliveriesDBManager deliveriesDBManager = DeliveriesDBManager.getInstance();
         try {
             deliveriesDBManager.setProgress(deliveryId, Integer.parseInt(progressValue));
@@ -69,6 +73,7 @@ public class DeliveriesResource {
     @POST
     @Path("{deliveryId}/assignee")
     public Response setAssignee(@PathParam("deliveryId") String deliveryId, String json) {
+        log.info("set delivery assignee");
         DeliveriesDBManager deliveriesDBManager = DeliveriesDBManager.getInstance();
         try {
             User user = User.deserialize(json, User.class);
@@ -83,6 +88,7 @@ public class DeliveriesResource {
     @POST
     @Path("{id}/comment/")
     public Response addComment(@PathParam("id") String deliveryId, String json) {
+        log.info("add delivery comment");
         DeliveriesDBManager deliveriesDBManager = DeliveriesDBManager.getInstance();
         try {
             Comment comment = Comment.deserialize(json, Comment.class);
