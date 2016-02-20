ce6e791173f73926e4bd6d113b294b40940422dd
Stanislav Smirnov <stas.smirnov88@gmail.com>
2016-02-20 22:44:49 +0300

    users: createUser endpoint

--- src/main/java/com/electric3/server/resources/users/UsersResource.java
+++ src/main/java/com/electric3/server/resources/users/UsersResource.java
@@ -1,8 +1,10 @@
 package com.electric3.server.resources.users;
 
+import com.electric3.dataatoms.User;
 import com.electric3.server.utils.StackTraceUtils;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -18,6 +20,20 @@ public class UsersResource {
         UsersDBManager usersDBManager = UsersDBManager.getInstance();
         try {
             return Response.ok(usersDBManager.getUsers(), MediaType.APPLICATION_JSON).build();
+        } catch (Exception e) {
+            log.severe(StackTraceUtils.getStackTrace(e));
+            return Response.serverError().build();
+        }
+    }
+
+    @POST
+    public Response createUser(String json) {
+        log.info("create user");
+        UsersDBManager usersDBManager = UsersDBManager.getInstance();
+        try {
+            User user = User.deserialize(json, User.class);
+            usersDBManager.createUser(user);
+            return Response.ok().build();
         } catch (Exception e) {
             log.severe(StackTraceUtils.getStackTrace(e));
             return Response.serverError().build();
